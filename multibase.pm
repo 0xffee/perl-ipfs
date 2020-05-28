364335b2ac88a7d3f8ab1b1067d5b6c2f9a85174
Bernhard M. Wiedemann <githubbmw+commit@lsmod.de>
2020-05-28 11:54:16 +0200

    Switch from strings to arrays
    reduce some code duplication
    add decodebase2

--- multibase.pm
+++ multibase.pm
@@ -5,9 +5,20 @@ our %codecs=qw(
 0 base2
 f base16
 b base32
+k base36
 z base58btc
 m base64
 );
+our %prefix_alphabets=(
+  0 => [ 0..1 ],
+  b => [ "a".."z", 2..7 ],
+  k => [ 0..9,"a".."z" ],
+  z => [ grep /[^IOl]/, 1..9,"A".."Z","a".."z" ],
+  m => [ "A".."Z", "a".."z", 0..9, "+", "/"],
+);
+
+#https://github.com/bmwiedemann/perl-ipfs/commit/83cb86c33eceafe50980ca0a1ed3409e8a8c2c9a#commitcomment-39496064
+
 our %rcodecs;
 for(keys(%codecs)) {$rcodecs{$codecs{$_}}=$_}
 
@@ -15,7 +26,7 @@ sub decodebasex($$$)
 { my($chars, $bitsperchar, $data) = @_;
     my $bits="";
     foreach my $c (split(//, $data)) {
-        my $d=index($chars, $c);
+        my $d=index(join("",@$chars), $c);
         $bits.=sprintf "%0${bitsperchar}B", $d;
     }
 #    die $bits;
@@ -32,29 +43,33 @@ sub encodebasex($$$)
         while (length($b)<$bitsperchar) {$b.="0"} # pad ending
         my $d=oct("0b".$b);
 #        print "$d = $b\n";
-        $res.=substr($chars, $d, 1);
+        $res.=$chars->[$d];
     }
 #    die $res;
     return $res;
 }
 sub decodebase64($)
-{decodebasex("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/", 6, $_[0])}
+{decodebasex($prefix_alphabets{m}, 6, $_[0])}
 sub encodebase64($)
-{encodebasex("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/", 6, $_[0])}
+{encodebasex($prefix_alphabets{m}, 6, $_[0])}
 sub decodebase32($)
-{decodebasex("abcdefghijklmnopqrstuvwxyz234567", 5, $_[0])}
+{decodebasex($prefix_alphabets{b}, 5, $_[0])}
 sub encodebase32($)
-{encodebasex("abcdefghijklmnopqrstuvwxyz234567", 5, $_[0])}
+{encodebasex($prefix_alphabets{b}, 5, $_[0])}
 sub encodebase16($)
 #{encodebasex("0123456789abcdef", 4, $_[0])}
 {unpack("H*", $_[0])}
+sub decodebase16($)
+{pack("H*", $_[0])}
 sub encodebase2($)
-{my $res=encodebasex("01", 1, $_[0]);
+{my $res=encodebasex($prefix_alphabets{0}, 1, $_[0]);
     $res=~s/^0*//;
     return $res;
 }
-sub decodebase16($)
-{pack("H*", $_[0])}
+sub decodebase2($)
+{my $in=shift; $in=("0" x ((-length($in))%8)).$in;
+    return decodebasex($prefix_alphabets{0}, 1, $in)
+}
 
 sub decode($)
 { my $in=shift;
@@ -85,6 +100,7 @@ sub test {
         die if encode("base32", $d) ne "bciqmtv6yrynmhodqoie6s2e73n3orfm2dzkdub66pielfdar6xk2aby";
         die if encode("base16", $d) ne "f1220c9d7d88e1ac3b8707209e9689fdb76e8959a1e543a07de7a08b28c11f5d5a007";
         die if encode("base2", "0") ne "0110000";
+        die if decode("0110000") ne "0";
         die if encode("base2", $d) ne "010010001000001100100111010111110110001000111000011010110000111011100001110000011100100000100111101001011010001001111111011011011101101110100010010101100110100001111001010100001110100000011111011110011110100000100010110010100011000001000111110101110101011010000000000111";
 }
 test();
